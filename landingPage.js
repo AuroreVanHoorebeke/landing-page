5e21f32c950670b4ded9e64c6df123a8d1fd21c6
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-17 19:18:02 +0100

    https links and comment cleaning

--- landingPage.js
+++ landingPage.js
@@ -1,9 +1,9 @@
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 const urlPic = "https://picsum.photos/v2/list";
-const urlWeather5 = "http://api.openweathermap.org/data/2.5/forecast?q=Brussels&appid=28f60020b1762edbb924e3df09acb1d4";
+const urlWeather5 = "https://api.openweathermap.org/data/2.5/forecast?q=Brussels&appid=28f60020b1762edbb924e3df09acb1d4";
 //id=524901&
-const urlWeather1 = "http://api.openweathermap.org/data/2.5/weather?q=Brussels&units=metric&appid=28f60020b1762edbb924e3df09acb1d4";
+const urlWeather1 = "https://api.openweathermap.org/data/2.5/weather?q=Brussels&units=metric&appid=28f60020b1762edbb924e3df09acb1d4";
 const date = document.querySelector(".date");
 const time = document.querySelector(".time");
 const copyright = document.querySelector(".copyright");
@@ -14,15 +14,6 @@ const feelsLikeTemp = document.querySelector(".feelsLikeTemp");
 const minima = document.querySelector(".minima");
 const maxima = document.querySelector(".maxima");
 const sky = document.querySelector(".sky");
-
-// function randImg(){
-//     // body.style.backgroundImage = `url(${arrElem.download_url})`
-//     // body.style.backgroundSize = "cover";
-//     // body.style.height= "100vh";
-//     // body.style.width= "100vw";
-//     document.body.style.backgroundImage = `url(${array[i].download_url})`
-//     setTimeout(randImg(), 1000)
-// }
 
 function displayTime(){
     const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
@@ -41,19 +32,7 @@ fetch(urlPic)
 .then (array => {
     console.log(array)
     let i = 0;
-    // for (let i = 0; i < array.length; i++){
-    //     console.log(array[i]);
-    //     let arrElem = array[i];
-    //     let imgUrl = array[i].download_url;
-    //     let imgId = array[i].id;
-    //     let imgWidth = array[i].width;
-    //     let imgHeight = array[i].height;
-    //     console.log(imgUrl);
-    //     console.log(imgId);
-    //     console.log(imgWidth);
-    //     console.log(imgHeight);
-    //     return arrElem
-    // }
+
     console.log(i);
     function setImg(){
         console.log(i);
